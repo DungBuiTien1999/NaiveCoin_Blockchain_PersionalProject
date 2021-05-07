4ad218be9c6543cc7c52a415b15a0c904884c7a2
DungBuiTien1999 <44043622+DungBuiTien1999@users.noreply.github.com>
2021-05-07 18:54:33 +0700

    create the structure and blockchain object

---
+++ src/blockchain.js
@@ -0,0 +1,27 @@
+
+
+class Block {
+    constructor(index, hash, previousHash, timestamp, data, difficulty, nonce) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.difficulty = difficulty;
+        this.nonce = nonce;
+    }
+}
+
+const genesisTransaction = {
+    'txIns': [{'txOutId': 0, 'txOutIndex': '', 'signature': ''}],
+    'txOuts': [{
+        'address': '04bfcab8722991ae774db48f934ca79cfb7dd991229153b9f732ba5334aafcd8e7266e47076996b55a14bf9913ee3145ce0cfc1372ada8ada74bd287450313534a',
+        'amount': 50
+    }],
+    'id': 'e655f6a5f26dc9b4cac6e46f52336428287759cf81ef5ff10854f69d68f43fa3'
+}
+
+const genesisBlock = new Block(0, '91a73664bc84c0baa1fc75ea6e4aa6d1d20c5df664c724e3159aefc2e1186627', '', 1465154705,[genesisTransaction], 0, 0);
+
+let blockchain = [genesisBlock];
+
