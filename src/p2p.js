7061a415787e06322c5d528b7a2eace5e8e85573
DungBuiTien1999 <44043622+DungBuiTien1999@users.noreply.github.com>
2021-05-08 16:50:08 +0700

    finish peer to peer

--- src/p2p.js
+++ src/p2p.js
@@ -1,8 +1,207 @@
-const WebSocket = require('ws');
-import {Server} from 'ws';
+const WebSocket = require("ws");
+import { write } from "fs";
+import { Server } from "ws";
 import {
-    addBlockToChain, Block, getBlockchain, getLatestBlock, handleReceivedTransaction, isValidBlockStructure,
-    replaceChain
-} from './blockchain';
-import {Transaction} from './transaction';
-import {getTransactionPool} from './transactionPool';
+  addBlockToChain,
+  Block,
+  getBlockchain,
+  getLatestBlock,
+  handleReceivedTransaction,
+  isValidBlockStructure,
+  replaceChain,
+} from "./blockchain";
+import { Transaction } from "./transaction";
+import { getTransactionPool } from "./transactionPool";
+
+const sockets = [];
+
+const MessageType = {
+  QUERY_LASTEST: 0,
+  QUERY_ALL: 1,
+  RESPONSE_BLOCKCHAIN: 2,
+  QUERY_TRANSACTION_POOL: 3,
+  RESPONSE_TRANSACTION_POOL: 4,
+};
+
+class Message {
+  constructor(type, data) {
+    this.type = type;
+    this.data = data;
+  }
+}
+
+const initP2PServer = (p2pPort) => {
+  const server = new WebSocket.Server({ port: p2pPort });
+  server.on("connection", (ws) => {
+    initConnection(ws);
+  });
+  console.log("listening websocket p2p port on: " + p2pPort);
+};
+
+const getSockets = () => sockets;
+
+const initConnection = (ws) => {
+  sockets.push(ws);
+  initMessageHandler(ws);
+  initErrorHandler(ws);
+  write(ws, queryChainLengthMsg());
+
+  //query transaction pool only some time after chain query
+  setTimeout(() => {
+    broadcast(queryTransactionPoolMsg());
+  }, 500);
+};
+
+const JSONToObject = (data) => {
+  try {
+    return JSON.parse(data);
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+};
+
+const initMessageHandler = (ws) => {
+  ws.on("message", (data) => {
+    try {
+      const message = JSONToObject(data);
+      if (message === null) {
+        console.log("could not parse received JSON message: " + data);
+        return;
+      }
+      console.log(`Received message: ${JSON.stringify(message)}`);
+      switch (message.type) {
+        case MessageType.QUERY_LASTEST:
+          write(ws, responseLastestMsg());
+          break;
+        case MessageType.QUERY_ALL:
+          write(ws, responseChainMsg());
+          break;
+        case MessageType.RESPONSE_BLOCKCHAIN:
+          const receivedBlocks = JSONToObject(message.data);
+          if (receivedBlocks === null) {
+            console.log(
+              `invalid blocks received: ${JSON.stringify(message.data)}`
+            );
+            break;
+          }
+          handleBlockchainResponse(receivedBlocks);
+          break;
+        case MessageType.QUERY_TRANSACTION_POOL:
+          write(ws, responseTransactionPoolMsg());
+          break;
+        case MessageType.RESPONSE_TRANSACTION_POOL:
+          const receivedTransactions = JSONToObject(message.data);
+          if (receivedTransactions === null) {
+            console.log(
+              `invalid transaction received: ${JSON.stringify(message.data)}`
+            );
+            break;
+          }
+          receivedTransactions.forEach((transaction) => {
+            try {
+              handleReceivedTransaction(transaction);
+              // if no error is thrown, transaction was indeed added to the pool
+              // let's broadcast transaction pool
+              broadCastTransactionPool();
+            } catch (e) {
+              console.log(e.message);
+            }
+          });
+          break;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+};
+
+const write = (ws, message) => ws.send(JSON.stringify(message));
+const broadcast = (message) =>
+  sockets.forEach((socket) => write(socket, message));
+
+const queryChainLengthMsg = () => ({
+  type: MessageType.QUERY_LASTEST,
+  data: null,
+});
+const queryAllMsg = () => ({ type: MessageType.QUERY_ALL, data: null });
+
+const responseChainMsg = () => ({
+  type: MessageType.RESPONSE_BLOCKCHAIN,
+  data: JSON.stringify(getBlockchain()),
+});
+
+const responseLastestMsg = () => ({
+    type: MessageType.RESPONSE_BLOCKCHAIN,
+    data: JSON.stringify([getLatestBlock()])
+});
+
+const queryTransactionPoolMsg = () => ({
+    type: MessageType.QUERY_TRANSACTION_POOL,
+    data: null
+});
+
+const responseTransactionPoolMsg = () => ({
+    type: MessageType.RESPONSE_TRANSACTION_POOL,
+    data: JSON.stringify(getTransactionPool())
+});
+
+const initErrorHandler = (ws) => {
+    const closeConnection = myWs => {
+        console.log(`connection failed to peer: ${myWs.url}`);
+        sockets.splice(sockets.indexOf(myWs), 1);
+    };
+    ws.on('close', () => closeConnection(ws));
+    ws.on('error', () => closeConnection(ws));
+};
+
+const handleBlockchainResponse = (receivedBlocks) => {
+    if(receivedBlocks.length === 0) {
+        console.log('received block chain size of 0');
+        return;
+    }
+    const lastestBlockReceived = receivedBlocks[receivedBlocks.length - 1];
+    if(!isValidBlockStructure(lastestBlockReceived)) {
+        console.log('block structuture not valid');
+        return;
+    }
+    const lastestBlockHeld = getLatestBlock();//lastestBblock of local
+    if(lastestBlockReceived.index > lastestBlockHeld.index) {
+        console.log(`blockchain possibly behind. We got: ${latestBlockHeld.index} Peer get ${latestBlockReceived.index}`);
+        if(lastestBlockHeld.hash === lastestBlockReceived.previousHash) {
+            if(addBlockToChain(lastestBlockReceived)) {
+                broadcast(responseLastestMsg());
+            }
+        } else if(receivedBlocks.length === 1) {
+            console.log('We have to query the chain from our peer');
+            broadcast(queryAllMsg());
+        } else {
+            console.log('Received blockchain is longer than current blockchain');
+            replaceChain(receivedBlocks);
+        }
+    } else {
+        console.log('received blockchain is not longer than received blockchain. Do nothing');
+    }
+};
+
+const broadcastLastest = () => {
+    broadcast(responseLastestMsg());
+};
+
+const connectToPeers = newPeer => {
+    const ws = new WebSocket(newPeer);
+    ws.on('open', () => {
+        initConnection(ws);
+    });
+    ws.on('error', () => {
+        console.log('connection failed');
+    });
+};
+
+const broadCastTransactionPool = () => {
+    broadcast(responseTransactionPoolMsg());
+};
+
+module.exports = {
+    connectToPeers, broadcastLastest, broadCastTransactionPool, initP2PServer, getSockets
+}
